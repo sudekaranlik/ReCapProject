178bdf94dae7ffee0ee9ca6e4de96adefa5cdafb
sude-1 <69909424+sude-1@users.noreply.github.com>
2021-10-13 13:45:05 +0300

    Add project files.

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,23 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+        //veri erişim entityframework veya başka bir durum olduğunda bile burada değişklik yapmaycağım çünkü interface kullandım
+        ICarDal _carDal;
+        public CarManager(ICarDal carDal)
+        {
+            _carDal = carDal;
+        }
+        public List<Car> GetAll()
+        {
+            return _carDal.GetAll();
+        }
+    }
+}
